e9afa13e71ac188ba75453ef885a0d421604e0fb
Adriana Morales <adrianasmoraleso@gmail.com>
2026-05-27 13:36:26 -0500

    chore/Ejercicio2:Se hizo la prueba para la clase CuentaBancaria haciendo uso de los getters y setters (secreó la clase fuera del paquete de Clases para probar las propiedades public y private)

---
+++ src/main/java/org/taller/PruebaCuentaBancaria.java
@@ -0,0 +1,17 @@
+public class PruebaCuentaBancaria {
+    public static void main(String[] args) {
+        CuentaBancaria cuenta = new CuentaBancaria("001-123456", 1500.0, "Ahorros");
+
+        cuenta.mostrarDetalles();
+        System.out.println();
+
+        cuenta.setSaldo(2000.0);
+        System.out.println("Saldo actualizado: $" + cuenta.getSaldo());
+
+        // Acceso permitido al atributo público tipoCuenta
+        System.out.println("Tipo de cuenta: " + cuenta.tipoCuenta);
+
+        // La siguiente línea causaría ERROR DE COMPILACIÓN porque numeroCuenta es privado:
+        // System.out.println(cuenta.numeroCuenta); // 'numeroCuenta' has private access
+    }
+}
